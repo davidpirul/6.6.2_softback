aae62ed9b8949fa8f08fbc99c1e9b981cfb31756
David Pirul <david.epa@live.com>
2023-02-07 00:30:38 -0300

    Update dataBase.js

--- src/db/dataBase.js
+++ src/db/dataBase.js
@@ -8,7 +8,6 @@ const baseDatos = {
 	"password": process.env.DB_PASSWORD,
 	"database": process.env.DB_DATABASE,
 	"host": process.env.DB_HOST,
-	"dialect": process.env.DB_DIALECT,
 	"port": process.env.DB_PORT,
 	allowExitOnIddle: true
 
